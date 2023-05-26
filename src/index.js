f8064ad1455c8e785436dbce2df0bf0dbbf8b59a
harsh <bhardwaj@gmail.com>
2023-05-26 13:30:15 -0700

    initial setup in reminder service

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const { PORT } = require("./config/serverConfig");
+const setupAndStartServer = () => {
+  app.use(bodyParser.json);
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.listen(PORT, () => {
+    console.log(`server is started at ${PORT}`);
+  });
+};
+
+setupAndStartServer();
