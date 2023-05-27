e5eb5c703a78dad2e4144bba5209cf48cf3c8cbf
harsh <bhardwaj@gmail.com>
2023-05-27 13:22:07 -0700

    added ticket model

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const { PORT } = require("./config/serverConfig");
+const cron = require("node-cron");
 const { sendBasicEmail } = require("./service/email-service");
 const setupAndStartServer = () => {
   app.use(bodyParser.json);
@@ -10,12 +11,16 @@ const setupAndStartServer = () => {
   app.listen(PORT, () => {
     console.log(`server is started at ${PORT}`);
 
-    sendBasicEmail(
-      "support@admin.com",
-      "coder9011@gmail.com",
-      "this is testing mail",
-      "harsh this side"
-    );
+    // sendBasicEmail(
+    //   "support@admin.com",
+    //   "coder9011@gmail.com",
+    //   "this is testing mail",
+    //   "harsh this side"
+    // );
+
+    cron.schedule("*/2 * * * *", () => {
+      console.log("this statment is running in two min.");
+    });
   });
 };
 
